a386579437bfd7433d11c325ae46db7e92081e1c
rbensua <rbensua@gmail.com>
2018-06-06 12:54:53 +0200

    add.alpha bug corrected

--- R/wavPlot.R
+++ R/wavPlot.R
@@ -167,3 +167,16 @@ wavPlot <-
   }
 
 }
+
+
+
+
+add.alpha <- function(COLORS, ALPHA){
+  if(missing(ALPHA)) stop("provide a value for alpha between 0 and 1")
+  RGB <- col2rgb(COLORS, alpha=TRUE)
+  RGB[4,] <- round(RGB[4,]*ALPHA)
+  NEW.COLORS <- rgb(RGB[1,], RGB[2,], RGB[3,], RGB[4,], maxColorValue = 255)
+  return(NEW.COLORS)
+}
+
+
